c4df31876456172e34cbee62cafcfce209f1890a
min-j <minj@bxscience.edu>
2020-07-21 14:15:20 +0000

    fetching data from db

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 events = [
         {"event":"First Day of Classes", "date":"2019-08-21"},
         {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
+        {"event":"Finals Begin", "date":"2019-12-01"},
         {"event": "Project Presentation", "date": "2020-07-31"}
     ]
 
@@ -25,7 +25,7 @@ app.config['MONGO_DBNAME'] = 'test'
 app.config['MONGO_URI'] = 'mongodb+srv://admin:epa0r89txUF9HQLb@cluster0.80lh6.mongodb.net/test?retryWrites=true&w=majority'
 
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 # -- Routes section --
 # INDEX
@@ -43,8 +43,10 @@ def index():
 
 def add():
     # connect to the database
-
+    collections = mongo.db.events
     # insert new data
-
+    # collections.insert({"event": "Birthday", "data": "2020-10-04"})
+    my_events = list(collections.find({}).sort("date", -1))
+    print(my_events)
     # return a message to the user
-    return ""
+    return "Event added successfully."
